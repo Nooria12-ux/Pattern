2807a44353162e27a1f616b087c377ab81970e50
Nooria <samihamaysoonnooria@gmail.com>
2025-02-14 16:06:49 +0600

    vdcv dc

---
+++ Mirror triangle.py
@@ -0,0 +1,12 @@
+rows = int(input("Enter number of rows: "))
+
+for i in range(1, rows + 1):
+    
+    for j in range(0, rows - i):
+        print(" ", end=" ")
+        
+    for j in range(1, i + 1):
+        print("❤️", end=" ")
+        
+    print()
+        
